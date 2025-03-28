5b0d6714f2f665cbe0f0362dc0f1dd48d8af29f7
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-03-28 16:20:38 +0100

    add model, views and urls to products

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
+    path('', views.all_products, name='products'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """ View to show all products, include search and sorting """
+    products = Product.objects.all()
+    print(products)
+    context = {
+        'products':products,
+    }
+
+    return render(request, 'products/products.html', context)
