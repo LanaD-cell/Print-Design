6a3c16b90ab06f4d70f2faed2e139d02eda06b47
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-03-31 12:37:46 +0200

    add product detail.html and responsiveness

--- products/models.py
+++ products/models.py
@@ -1,15 +1,25 @@
+"""
+Create models for Product and Category in Admin.
+"""
 from django.db import models
 
 
 class Category(models.Model):
+    """
+    Create Categroy for search functionality in Admin and site
+    """
 
     class Meta:
         verbose_name_plural = "Categories"
+    """
+    Adress plural error of Category in Admin
+    """
 
     name = models.CharField(max_length=255)
     friendly_name = models.CharField(max_length=255, null=True, blank=True)
     subcategories = models.ManyToManyField(
-        'self', blank=True, related_name='parent_categories', symmetrical=False)
+        'self', blank=True,
+        related_name='parent_categories', symmetrical=False)
 
     def __str__(self):
         return str(self.name)
@@ -29,10 +39,17 @@ class Product(models.Model):
         max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     fallback_url = models.CharField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+
 
     def __str__(self):
         return str(self.name)
 
+
     def get_image(self):
-        return self.image_url if self.image_url else self.fallback_url
+        """ Image backup for Cloudinary (not yet implemented)"""
+        if self.image_url:
+            return self.image_url
+        elif self.fallback_url:
+            return self.fallback_url
+        return '/path/to/default/image.jpg'
