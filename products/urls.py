3aa60bb023c1556c259394c5966bdb81100cd9c2
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-04-02 13:05:10 +0200

    add main-nav product details

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import( product_detail,
                    all_products,
                    category_detail,
-                   terms_and_conditions)
+                   terms_and_conditions,
+                   main_nav)
 
 
 urlpatterns = [
@@ -10,4 +11,5 @@ urlpatterns = [
     path('terms/', terms_and_conditions, name='terms'),
     path('product/<int:product_id>/', product_detail, name='product_detail'),
     path('category/<int:category_id>/', category_detail, name='category_detail'),
+    path('main-nav/', main_nav, name='main_nav'),
 ]
