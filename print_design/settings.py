625a693d5928d066538b79207a012565f1953ece
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-04-02 15:02:45 +0200

    add bag app and setup

--- print_design/settings.py
+++ print_design/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [
 # Application definition
 INSTALLED_APPS = [
     'cloudinary',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,7 +45,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'homepage',
     'products',
-    'jazzmin',
+    'bag',
 ]
 
 MIDDLEWARE = [
