16136a8b6832ad9ffbfea5dd08d30d6eaa54fb16
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-04-03 13:19:48 +0200

    add seal bar to home page and FAQ section

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,7 +1,15 @@
+from ckeditor.widgets import CKEditorWidget
 from django.contrib import admin
 from .models import FAQ
+from django import forms
 
+class FAQForm(forms.ModelForm):
+    class Meta:
+        model = FAQ
+        fields = '__all__'
+    answer = forms.CharField(widget=CKEditorWidget())
 
 @admin.register(FAQ)
 class FAQAdmin(admin.ModelAdmin):
     list_display = ('question',)
+    form = FAQForm
