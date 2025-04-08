fe42a9a143a71ab88ec84774409523c5a25f2ba1
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-04-08 09:42:17 +0200

    add cart htmls

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
     path('', views.view_cart, name='view_cart'),
-    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('add/<item_id>/', views.add_to_cart, name='add_to_bag'),
+    path('adjust/<item_id>/', views.adjust_cart, name='adjust_bag'),
+    path('remove/<item_id>/', views.remove_from_cart, name='remove_from_bag'),
+    path('account/login/', auth_views.LoginView.as_view(), name='login'),
+    path('account/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
