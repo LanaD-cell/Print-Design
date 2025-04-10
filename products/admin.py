260b3d622fa34dc27eb5febdd97d1021509992df
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-04-10 16:21:26 +0200

    add cart models and admin

--- products/admin.py
+++ products/admin.py
@@ -1,42 +1,8 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import (Product,
-                    Category,
-                    ProductSize,
-                    QuantityOption)
+from .models import Product, Category
+
 from django import forms
-
-
-class QuantityOptionForm(forms.ModelForm):
-    class Meta:
-        model = QuantityOption
-        fields = ['quantity', 'price']
-
-    def clean(self):
-        cleaned_data = super().clean()
-        quantity = cleaned_data.get('quantity')
-        price = cleaned_data.get('price')
-
-        if not quantity or not price:
-            raise forms.ValidationError("Both quantity and price must be provided.")
-
-        # Ensure price is greater than zero
-        if price <= 0:
-            raise forms.ValidationError("Price must be greater than zero.")
-
-        return cleaned_data
-
-
-class ProductSizeInline(admin.TabularInline):
-    model = ProductSize
-    extra = 1
-
-
-class QuantityOptionInline(admin.TabularInline):
-    model = QuantityOption
-    form = QuantityOptionForm
-    extra = 1
-    fields = ['quantity', 'price']
 
 
 class ProductAdmin(SummernoteModelAdmin):
@@ -54,8 +20,6 @@ class ProductAdmin(SummernoteModelAdmin):
     # Add Summernote editor for 'description' field
     summernote_fields = ('description',)
 
-    inlines = [ProductSizeInline, QuantityOptionInline]
-
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
 
@@ -72,5 +36,3 @@ class CategoryAdmin(admin.ModelAdmin):
 # Register the models with the admin
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(ProductSize)
-admin.site.register(QuantityOption)
