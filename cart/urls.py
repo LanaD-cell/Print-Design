af37a10931c48fd89e5849dd6a3f7693a493e797
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-04-11 10:39:34 +0200

    fix cart errors

--- cart/urls.py
+++ cart/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'cart'
 
 urlpatterns = [
-    path('', views.view_cart, name='cart'),
+    path('', views.view_cart, name='cart_details'),
     path('add/', views.add_to_cart, name='add_to_cart'),
-    path('checkout/', views.create_order, name='create_order'),
-
+    path('checkout/', views.create_order, name='create_cart_order'),
 ]
--- print_design/urls.py
+++ print_design/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', include('homepage.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
+    path('login/', include('django.contrib.auth.urls')),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
